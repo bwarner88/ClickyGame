5f27e54305e7756a90157761b569a0632abdd609
unknown <brian_warner25@yahoo.com>
2018-09-08 21:17:49 -0700

    finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,7 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer">
-    <div className="bottom">
-      Clicky Game! <img alt="react" src="assets/images/react.svg" />
-    </div>
+    <div className="bottom"></div>
   </footer>
 );
 
